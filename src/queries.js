c204df5fe81cc1cb4eee79b701bc0ce4e40b5e16
hamedaravane <hamedaravane@gmail.com>
2022-08-16 00:43:02 +0430

    optimizing queries

--- src/queries.js
+++ src/queries.js
@@ -1,66 +1,64 @@
 export const allObjkts = `query MyQuery {
-    event(order_by: {timestamp: desc}, where: {event_type: {_eq: "ask_purchase"}}, limit: 24) {
-      amount
-      creator {
-        address
-        alias
-        twitter
-      }
-      event_type
-      price
-      token {
-        artifact_uri
-        average
-        decimals
-        display_uri
-        highest_offer
-        last_listed
-        last_metadata_update
-        level
-        lowest_ask
-        metadata
-        name
-        supply
-        thumbnail_uri
-        timestamp
-        pk
-        royalties {
-          amount
-          decimals
-          token {
-            artifact_uri
-            average
-            decimals
-            description
-            display_uri
-            extra
-            flag
-            highest_offer
-            is_boolean_amount
-            last_listed
-            last_metadata_update
-            level
-            lowest_ask
-            metadata
-            mime
-            name
-            ophash
-            rights
-            supply
-            symbol
-            thumbnail_uri
-            timestamp
-            tzip16_key
-          }
-        }
-        token_id
-      }
-      fa_contract
-      fa {
-        name
-      }
-      marketplace {
-        name
-      }
+  event(order_by: {timestamp: desc}, where: {event_type: {_eq: "ask_purchase"}, token: {supply: {_lte: "50"}}}, limit: 36) {
+    amount
+    creator {
+      address
+      alias
+      twitter
     }
-  }`
+    event_type
+    price
+    token {
+      average
+      decimals
+      highest_offer
+      last_listed
+      last_metadata_update
+      level
+      lowest_ask
+      metadata
+      name
+      supply
+      thumbnail_uri
+      timestamp
+      pk
+      royalties {
+        amount
+        decimals
+        token {
+          artifact_uri
+          average
+          decimals
+          description
+          display_uri
+          extra
+          flag
+          highest_offer
+          is_boolean_amount
+          last_listed
+          last_metadata_update
+          level
+          lowest_ask
+          metadata
+          mime
+          name
+          ophash
+          rights
+          supply
+          symbol
+          thumbnail_uri
+          timestamp
+          tzip16_key
+        }
+      }
+      token_id
+    }
+    fa_contract
+    fa {
+      name
+    }
+    marketplace {
+      name
+    }
+  }
+}`
